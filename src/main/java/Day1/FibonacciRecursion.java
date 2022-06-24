71e2000eaaae3cc10458bf730ae256e34e969fbb
KushangMistry <kushangmistry283@gmail.com>
2022-06-24 09:12:36 -0300

    Added comments for better understanding of the problems

--- src/main/java/Day1/FibonacciRecursion.java
+++ src/main/java/Day1/FibonacciRecursion.java
@@ -1,8 +1,25 @@
+/**
+ * A program to print n fibonacci numbers
+ * Using recursion functionality
+ *
+ * Expected Input1: 2
+ * Expected Output1: 0 1
+ *
+ * Expected Input2: 5
+ * Expected Output2: 0 1 1 2 3
+ *
+ * Expected Input3: 9
+ * Expected Output3: 0 1 1 2 3 5 8 13 21
+ *
+ * Author: Kushang Mistry
+ * Date: June 23, 2022
+ * */
+
 package Day1;
 
 public class FibonacciRecursion {
     public static void main(String[] args) {
-        int totalTerms = 10;
+        int totalTerms = 9;
         System.out.println("0");
         System.out.println("1");
         GenerateFibonacci(0, 1, totalTerms - 2);
